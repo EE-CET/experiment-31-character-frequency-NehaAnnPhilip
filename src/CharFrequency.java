b759edd18998ab46fe264c9c4e9b45405485064c
NehaAnnPhilip <nehaphilip0205@gmail.com>
2026-04-06 00:20:37 +0530

    Update CharFrequency.java

--- src/CharFrequency.java
+++ src/CharFrequency.java
@@ -1,10 +1,28 @@
+
 import java.util.Scanner;
 
 public class CharFrequency {
-    
-        // TODO: Read the string S (Use sc.nextLine() to capture spaces)
-        // TODO: Read the character C
-        // TODO: Iterate through the string and count occurrences of C
-        // TODO: Print the count
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        // Input string
+        String S = sc.nextLine();
+
+        // Input character
+        char C = sc.next().charAt(0);
+
+        int count = 0;
+
+        // Loop through the string
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == C) {
+                count++;
+            }
+        }
+
+        // Output result
+        System.out.println(count);
+
+        sc.close();
+    }
 }
